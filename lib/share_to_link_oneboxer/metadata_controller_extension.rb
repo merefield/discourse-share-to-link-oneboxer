b0fa4c4f70bbfefadbbf9086048680cf354b6b9a
merefield <merefield@gmail.com>
2025-05-07 21:25:52 +0100

    experimental change

--- lib/share_to_link_oneboxer/metadata_controller_extension.rb
+++ lib/share_to_link_oneboxer/metadata_controller_extension.rb
@@ -4,8 +4,8 @@ module ::ShareToLinkOneboxer
   module MetadataControllerExtension
     def default_manifest
       super.tap do |manifest|
-        manifest[:share_target][:params][:text] = 'body'
-        manifest[:share_target][:params][:title] = 'body'
+        # manifest[:share_target][:params][:text] = 'body'
+        manifest[:share_target][:params][:title] = "link"
       end
     end
   end
